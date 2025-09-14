49e3e67314e5cf1751f4433bd8a6f0496c0eb805
nani-tyson <nanityson10@gmail.com>
2025-09-14 13:53:06 +0000

    backend setup completed

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+//configs
+import connectDB from './src/config/db.js';
+
+//routes
+
+const app = express();
+dotenv.config();
+app.use(cors({
+    origin: [process.env.LOCALHOST_FRONTEND_URL, process.env.PRODUCTION_FRONTEND_URL],
+    credentials: true,
+}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
